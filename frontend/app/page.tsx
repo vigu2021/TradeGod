5c797e2d6e8c8a1c9fca7081d9781b7ea495e4b6
Vignesh Roachthavilit <vigneshroachthavilit@gmail.com>
2026-04-26 14:24:09 +0700

    Set up frontend via next.js

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-1 items-center justify-center">
+      <h1 className="text-2xl font-semibold">TradeGod</h1>
+    </main>
+  );
+}
